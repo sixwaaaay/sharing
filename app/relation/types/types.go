8059e8aa27667efcce4d6d03bb5c1a8709e92675
sixway <lw.2675@qq.com>
2022-05-28 20:49:58 +0800

    [feat] type definition of request and response for type safe

---
+++ app/relation/types/types.go
@@ -0,0 +1,39 @@
+package types
+
+type ActionReq struct {
+	UserId     string `form:"user_id" json:"user_id" binding:"required"`
+	Token      string `form:"token" json:"token" binding:"required"`
+	ToUserId   string `form:"to_user_id" json:"to_user_id" binding:"required"`
+	ActionType string `form:"action_type" json:"action_type" binding:"required"`
+}
+
+type ActionResp struct {
+	StatusCode int64  `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  string `json:"status_msg"`  // 返回状态描述
+}
+
+type RelationReq struct {
+	UserId string `form:"user_id" json:"user_id" binding:"required"`
+	Token  string `form:"token" json:"token" binding:"required"`
+}
+
+type FollowListResp struct {
+	StatusCode string  `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
+	UserList   []User  `json:"user_list"`   // 用户信息列表
+}
+
+type FollowerListResp struct {
+	StatusCode string  `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
+	UserList   []User  `json:"user_list"`   // 用户信息列表
+}
+
+// User 用户信息
+type User struct {
+	FollowCount   int64  `json:"follow_count"`   // 关注总数
+	FollowerCount int64  `json:"follower_count"` // 粉丝总数
+	ID            int64  `json:"id"`             // 用户id
+	IsFollow      bool   `json:"is_follow"`      // true-已关注，false-未关注
+	Name          string `json:"name"`           // 用户名称
+}
