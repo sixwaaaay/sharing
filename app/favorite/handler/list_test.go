f5aaaa3943e2b4f766a0eecfcdb5ab031f561609
sixway <lw.2675@qq.com>
2022-06-02 22:37:23 +0800

    [feat and fix] handler for favorite action and get favorite list 获取用户点赞列表handler 点赞操作handler 修正类型定义

---
+++ app/favorite/handler/list_test.go
@@ -0,0 +1,74 @@
+package handler
+
+import (
+	"bytelite/app/favorite/logic"
+	"bytelite/app/favorite/types"
+	"bytelite/common/errorx"
+	"bytelite/common/testhelper"
+	"bytelite/service"
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/zeromicro/go-zero/core/logx"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestFavoriteListHandler(t *testing.T) {
+	// 模拟逻辑层
+	logic.NewFavoriteListLogic = func(ctx context.Context, appCtx *service.AppContext) logic.FavoriteListLogic {
+		return func(req *types.FavoriteListReq) (resp *types.FavoriteListResp, err error) {
+			logx.WithContext(ctx).Infof("req: %+v", req)
+			if req.UserId == 101 {
+				//raise error
+				return nil, errorx.NewDefaultError("test error")
+			}
+			resp = &types.FavoriteListResp{
+				StatusCode: 0,
+				StatusMsg:  nil,
+				VideoList:  nil,
+			}
+			return resp, nil
+		}
+	}
+
+	var testCases = []testhelper.TestCase{
+		{
+			Name:   "biz logic success", // 测试点赞列表，业务逻辑成功
+			Method: "GET",
+			Path:   "/douyin/favorite/list/",
+			Form: url.Values{
+				"user_id": {"100"},
+				"token":   {"token"},
+			},
+			Expected: `{"status_code":0,"status_msg":null,"video_list":null}`,
+		},
+		{
+			Name:   "biz logic fail", // 测试点赞列表，业务逻辑失败, 返回错误信息
+			Method: "GET",
+			Path:   "/douyin/favorite/list/",
+			Form: url.Values{
+				"user_id": {"101"},
+				"token":   {"token"},
+			},
+			Expected: `{"status_code":1001,"status_msg":"test error","video_list":null}`,
+		},
+		{
+			Name:   "params error", // 参数错误
+			Method: "GET",
+			Path:   "/douyin/favorite/list/",
+			Form: url.Values{
+				"user_id": {"douyin"},
+			},
+			Expected: `{"status_code":1001,"status_msg":"invalid params","video_list":null}`,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name+" "+testCase.Method+" "+testCase.Path, func(t *testing.T) {
+			w, _ := testhelper.GenRequest(r, testCase.Method, testCase.Path, testCase.Body, testCase.Form)
+			assert.Equal(t, http.StatusOK, w.Code)
+			assert.JSONEq(t, testCase.Expected, w.Body.String())
+			t.Logf("%s", w.Body.String())
+		})
+	}
+}
