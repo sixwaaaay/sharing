d7fe7eec4d584a08193666e9da53c944248b6161
sixway <lw.2675@qq.com>
2022-06-08 00:19:08 +0800

    [feat] token验证中间件、权限验证中间件及相关测试

---
+++ common/middleware/auth.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"bytelite/service"
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+const UserClaimsKey = "user"
+
+type Auth struct {
+	Token *string `form:"token" json:"token" binding:"required"`
+}
+
+// VerifyToken 将 token 转化为 user_id 如果可行的话
+func VerifyToken(appCtx *service.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var auth Auth
+		if err := c.ShouldBind(&auth); err != nil {
+			c.Next()
+		}
+		if auth.Token == nil {
+			c.Next()
+			return
+		}
+		userId, err := appCtx.JWTSigner.ValidateToken(*auth.Token)
+		if err != nil {
+			logx.Error(err)
+			c.AbortWithStatus(401)
+			return
+		}
+		logx.Infof("userId: %d", userId)
+		ctx := context.WithValue(c.Request.Context(), UserClaimsKey, userId)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
+
+// Authority 过滤请求
+func Authority(appCtx *service.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		_, ok := c.Request.Context().Value(UserClaimsKey).(int64)
+		if !ok {
+			c.AbortWithStatus(401)
+			return
+		}
+	}
+}
---
+++ common/middleware/auth_test.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"bytelite/common/auth"
+	"bytelite/common/testhelper"
+	"bytelite/service"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	"net/url"
+	"testing"
+)
+
+func TestVerify(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+	context := service.AppContext{
+		JWTSigner: auth.NewJWTSigner("secret"),
+	}
+	r.Use(VerifyToken(&context))
+	var testUserId int64 = 25
+	token, err := context.JWTSigner.GenerateToken(testUserId, 60*60*24*7)
+	require.NoError(t, err)
+
+	r.GET("/test", func(c *gin.Context) {
+		ctx := c.Request.Context()
+		value := ctx.Value(UserClaimsKey).(int64)
+		require.Equal(t, testUserId, value)
+		c.String(200, "test")
+	})
+	w, _ := testhelper.GenRequest(r, "GET", "/test", nil, url.Values{
+		"token": {token},
+	})
+	require.Equal(t, w.Code, 200)
+}
