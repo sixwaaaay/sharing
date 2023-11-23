027825d97f650fe7873955fea5323a52d4186333
sixwaaaay <lw.2675@qq.com>
2023-11-23 23:55:31 +0800

    initial commit

---
+++ src/main/java/io/sixwaaaay/sharingcomment/SharingCommentApplication.java
@@ -0,0 +1,12 @@
+package io.sixwaaaay.sharingcomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingCommentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SharingCommentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/sixwaaaay/sharingcomment/SharingCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sixwaaaay.sharingcomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingCommentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
