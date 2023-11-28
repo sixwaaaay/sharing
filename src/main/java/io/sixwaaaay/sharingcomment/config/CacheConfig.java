2ffb28c6121d4d60e3e011de4c6aafe284bcc5d3
sixwaaaay <lw.2675@qq.com>
2023-11-29 00:01:44 +0800

    fix: add type specified mapper

--- src/main/java/io/sixwaaaay/sharingcomment/config/CacheConfig.java
+++ src/main/java/io/sixwaaaay/sharingcomment/config/CacheConfig.java
@@ -7,25 +7,29 @@ package io.sixwaaaay.sharingcomment.config;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.sixwaaaay.sharingcomment.domain.Comment;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
+import java.util.List;
 
 @Configuration(proxyBeanMethods = false)
 public class CacheConfig {
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer(ObjectMapper objectMapper) {
+        var type = objectMapper.getTypeFactory().constructCollectionType(List.class, Comment.class);
+
         var redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(6))
                 .disableCachingNullValues()
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer(objectMapper)
+                                new Jackson2JsonRedisSerializer<List<Comment>>(objectMapper, type)
                         )
                 );
         return (builder) -> builder
