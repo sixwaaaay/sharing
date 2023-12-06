e8618d5d0e6b7007fbb967e4b1c72b6caa231bb6
sixwaaaay <lw.2675@qq.com>
2023-12-06 22:45:58 +0800

    feat: add sign verification

--- src/main/java/io/sixwaaaay/sharingcomment/request/Principal.java
+++ src/main/java/io/sixwaaaay/sharingcomment/request/Principal.java
@@ -5,9 +5,13 @@
 
 package io.sixwaaaay.sharingcomment.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Principal {
     private String name;
     private Long id;
--- src/main/java/io/sixwaaaay/sharingcomment/util/TokenParser.java
+++ src/main/java/io/sixwaaaay/sharingcomment/util/TokenParser.java
@@ -5,34 +5,39 @@
 
 package io.sixwaaaay.sharingcomment.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import io.sixwaaaay.sharingcomment.request.Principal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Base64;
 import java.util.Optional;
 
 
 @Component
 public class TokenParser {
+    private final JwtParser jwtParser;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    public TokenParser() {
-        objectMapper.configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    public TokenParser(@Value("${jwt.secret}") String secret) {
+        var secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        jwtParser = Jwts.parser()
+                .verifyWith(secretKey)
+                .build();
     }
 
+    /**
+     * Parse a token and return a Principal object
+     * @param token Bearer token
+     * @return Optional of Principal
+     */
     public Optional<Principal> parse(String token) {
-        try {
-            byte[] authorizations = Optional.ofNullable(token)
-                    .map(s -> s.split("\\."))
-                    .filter(strings -> strings.length == 3)
-                    .map(strings -> strings[1])
-                    .map(Base64.getDecoder()::decode).orElseGet(() -> new byte[0]);
-            return Optional.ofNullable(objectMapper.readValue(authorizations, Principal.class));
-        } catch (Exception e) {
-            return Optional.empty();
-        }
+        if (token == null || token.isEmpty()) return Optional.empty();
+
+        var claimsJws = jwtParser.parseSignedClaims(token);
+        var name = claimsJws.getPayload().get("name", String.class);
+        var id = claimsJws.getPayload().get("id", String.class);
+        return Optional.of(new Principal(name, Long.parseLong(id)));
     }
 
 }
