9aa8343a8f699ec2b5a918558824da7998d526c0
sixwaaaay <lw.2675@qq.com>
2024-01-03 16:30:14 +0800

    feat: add endpoint

---
+++ sharp/content.Tests/repository/MapperTest.cs
@@ -0,0 +1,33 @@
+﻿using content.domainservice;
+using content.repository;
+using Xunit.Abstractions;
+
+namespace content.Tests.repository;
+
+public class MapperTest(ITestOutputHelper testOutputHelper)
+{
+    [Fact(DisplayName = "Video Mapper")]
+    public void Test2()
+    {
+        var video = new Video
+        {
+            Id = 1,
+            UserId = 1,
+            Title = "title",
+            Des = "des",
+            CoverUrl = "coverUrl",
+            VideoUrl = "videoUrl",
+            Duration = 1,
+            Category = "category",
+            Tags = "tags",
+            ViewCount = 1,
+            LikeCount = 1,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now,
+            Processed = 1
+        };
+        var videoDto = video.ToDto();
+        Assert.NotNull(videoDto);
+        testOutputHelper.WriteLine(videoDto.ToString());
+    }
+}
--- sharp/content/Program.cs
+++ sharp/content/Program.cs
@@ -1,5 +1,11 @@
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
+using content.domainservice;
+using content.endpoints;
+using content.repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MySqlConnector;
 
 var builder = WebApplication.CreateSlimBuilder(args);
@@ -7,14 +13,54 @@ var builder = WebApplication.CreateSlimBuilder(args);
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
 });
+
+var secret = builder.Configuration.GetSection("Secret").Value?? throw new InvalidOperationException("Secret is null");
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(
+    option =>
+    {
+        option.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(secret));
+        option.TokenValidationParameters.ValidateAudience = false;
+        option.TokenValidationParameters.ValidateIssuer = false;
+    }
+);
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddProblemDetails();
+
 builder.Services.AddMySqlDataSource(builder.Configuration.GetConnectionString("Default") ??
                                     throw new InvalidOperationException("Connection string is null"));
+builder.Services.AddVideoRepository();
 
+builder.Services.AddGrpcUser();
+builder.Services.AddUserRepository();
+
+builder.Services.AddVoteClient();
+builder.Services.AddVoteRepository();
+
+builder.Services.AddDomainService();
+builder.Services.AddEndpoints();
+builder.Services.AddResponseCompression();
 var app = builder.Build();
+
+app.UseResponseCompression();
+app.UseAuthorization();
+
+app.UseToken();
+
+app.MapEndpoints();
+
 app.Run();
 
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower)]
 [JsonSerializable(typeof(ProblemDetails))]
+[JsonSerializable(typeof(VideoRequest))]
+[JsonSerializable(typeof(VoteRequest))]
+[JsonSerializable(typeof(VideoDto))]
+[JsonSerializable(typeof(Pagination<VideoDto>))]
+[JsonSerializable(typeof(IReadOnlyList<VideoDto>))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext;
