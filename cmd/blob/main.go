a92e4cb42fbdd619add4da9d1b6b60d4d169dc0f
sixwaaaay <lw.2675@qq.com>
2024-01-08 22:47:39 +0800

    doc: update comments

--- cmd/blob/main.go
+++ cmd/blob/main.go
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2024 sixwaaaay.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main
 
 import (
@@ -14,6 +27,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/sixwaaaay/must"
 	"github.com/spf13/viper"
--- cmd/shauser/scripts/schema.sql
+++ cmd/shauser/scripts/schema.sql
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 sixwaaaay.
+ * Copyright (c) 2023-2024 sixwaaaay.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,6 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 DROP TABLE IF EXISTS users;
 
 DROP TABLE IF EXISTS follows;
