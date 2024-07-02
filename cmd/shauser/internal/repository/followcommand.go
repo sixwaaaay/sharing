dc9e74c5f579e3bbbcdee0ccbb8e102a337a4a09
sixwaaaay <lw.2675@qq.com>
2024-07-02 15:10:19 +0800

    feat: remove aggregation

--- cmd/shauser/internal/repository/followcommand.go
+++ cmd/shauser/internal/repository/followcommand.go
@@ -28,9 +28,9 @@ type FollowCommand interface {
 // followCommand is a struct that represents a command to follow a user.
 // It contains a database connection and a unique ID generator.
 type followCommand struct {
-	// db is a pointer to a gorm.DB object, which represents a database connection.
+	/* db is a pointer to a gorm.DB object, which represents a database connection. */
 	db *gorm.DB
-	// uniqueID is a pointer to a sonyflake.Sonyflake object, which is used to generate unique IDs.
+	/* uniqueID is a pointer to a sonyflake.Sonyflake object, which is used to generate unique IDs. */
 	uniqueID *sonyflake.Sonyflake
 }
 
@@ -38,9 +38,9 @@ type followCommand struct {
 // It takes a database connection as an argument and returns a pointer to a followCommand object.
 func NewFollowCommand(db *gorm.DB) FollowCommand {
 	return &followCommand{
-		// Initialize the db field with the provided database connection.
+		/* Initialize the db field with the provided database connection. */
 		db: db,
-		// Initialize the uniqueID field with a new Sonyflake object.
+		/* Initialize the uniqueID field with a new Sonyflake object. */
 		uniqueID: sonyflake.NewSonyflake(sonyflake.Settings{}),
 	}
 }
@@ -49,28 +49,18 @@ func NewFollowCommand(db *gorm.DB) FollowCommand {
 // It takes a context and a pointer to a Follow object as arguments.
 // It returns an error if the insertion fails.
 func (c *followCommand) Insert(ctx context.Context, f *Follow) (err error) {
-	// Generate a new unique ID.
+	/* Generate a new unique ID. */
 	id, err := c.uniqueID.NextID()
 	if err != nil {
 		return err
 	}
-	// Set the ID of the Follow object to the generated ID.
+	/* Set the ID of the Follow object to the generated ID. */
 	f.ID = int64(id)
 
-	// Start a new database transaction.
+	/* Start a new database transaction. */
 	return c.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
-		// Insert the Follow object into the database.
-		if err := tx.Create(f).Error; err != nil {
-			return err
-		}
-		// Increment the follow count of the user who is following.
-		res := tx.Raw("UPDATE users SET following = following + 1 WHERE id = ?", f.UserID)
-		if res.Error != nil {
-			return res.Error
-		}
-		// Increment the follower count of the user who is being followed.
-		res = tx.Raw("UPDATE users SET followers = followers + 1 WHERE id = ?", f.Target)
-		return res.Error
+		/* Insert the Follow object into the database. */
+		return tx.Create(f).Error
 	})
 }
 
@@ -78,23 +68,16 @@ func (c *followCommand) Insert(ctx context.Context, f *Follow) (err error) {
 // It takes a context and two user IDs as arguments: the ID of the user who is following and the ID of the user who is being followed.
 // It returns an error if the deletion fails.
 func (c *followCommand) Delete(ctx context.Context, userid, followTo int64) error {
-	// Start a new database transaction.
+	/* Start a new database transaction. */
 	err := c.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
-		// Delete the follow relationship from the database.
+		/* Delete the follow relationship from the database. */
 		res := tx.Exec("DELETE FROM follows WHERE user_id = ? AND target = ?", userid, followTo)
 		if res.Error != nil {
 			return res.Error
-		} else if res.RowsAffected == 0 { // If no rows were affected by the deletion, return an error.
+		} else if res.RowsAffected == 0 { /* If no rows were affected by the deletion, return an error. */
 			return gorm.ErrRecordNotFound
 		}
-		// Decrement the follow count of the user who was following.
-		res = tx.Exec("UPDATE users SET following = following - 1 WHERE id = ?", userid)
-		if res.Error != nil {
-			return res.Error
-		}
-		// Decrement the follower count of the user who was being followed.
-		res = tx.Exec("UPDATE users SET followers = followers - 1 WHERE id = ?", followTo)
-		return res.Error
+		return nil
 	})
 	return err
 }
